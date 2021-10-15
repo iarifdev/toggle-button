feba3f08e1365a1b4a577631bb00269b6c1970ac
Muhammad Arif <talentedarif@gmail.com>
2021-10-16 00:05:58 +0500

    done

---
+++ script.js
@@ -0,0 +1,22 @@
+const toggles = document.querySelector('toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
+
+toggles.forEach(toggle => toggle.addEventListener('change', (e) => doTheTrick(e.target)))
+
+function doTheTrick(){
+    if(good.checked && cheap.checked && fast.checked){
+        if(good === theClickedOne){
+            fast.checked = false;
+        }
+
+        if(cheap === theClickedOne){
+            good.checked = false;
+        }
+
+        if(fast === theClickedOne){
+            cheap.checked = false;
+        }
+    }
+}
